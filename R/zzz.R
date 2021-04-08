5f4b083118519583b7ddbac109263f8ff14d2dff
Haizi Zheng <haizi.zh@gmail.com>
2021-04-08 13:53:59 -0400

    Change the package name to Rhtslib12

--- R/zzz.R
+++ R/zzz.R
@@ -17,7 +17,7 @@ pkgconfig <- function(opt=c("PKG_LIBS", "PKG_CPPFLAGS"))
     if (opt == "PKG_LIBS") {
         usrlib_dir <- Sys.getenv(
             "RHTSLIB_RPATH",
-            system.file("usrlib", package="Rhtslib", mustWork=TRUE)
+            system.file("usrlib", package="Rhtslib12", mustWork=TRUE)
         )
         platform <- Sys.info()[["sysname"]]
         if (platform == "Windows") {
@@ -27,7 +27,7 @@ pkgconfig <- function(opt=c("PKG_LIBS", "PKG_CPPFLAGS"))
         usrlib_path <- sprintf("'%s'", file.path(usrlib_dir, "libhts.a"))
         if (platform == "Windows") {
             LOCAL_SOFT <- .getRconfig("LOCAL_SOFT", default="C:/extsoft")
-            ## See how PKG_LIBS is defined in Rhtslib/src/Makevars.win
+            ## See how PKG_LIBS is defined in Rhtslib12/src/Makevars.win
             ## and make sure to produce the same value here.
             libs <- c("curl", "rtmp", "ssl", "ssh2", "crypto",
                       "gdi32", "z", "ws2_32", "wldap32", "winmm")
@@ -36,19 +36,19 @@ pkgconfig <- function(opt=c("PKG_LIBS", "PKG_CPPFLAGS"))
             libs <- paste(sprintf("-l%s", libs), collapse=" ")
             libs <- sprintf("-L%s/lib/%s %s", LOCAL_SOFT, r_arch, libs)
         } else {
-            ## See how PKG_LIBS is defined in Rhtslib/src/Makevars
+            ## See how PKG_LIBS is defined in Rhtslib12/src/Makevars
             ## and make sure to produce the same value here.
             libs <- "-lcurl"
         }
         config <- paste(usrlib_path, libs)
     } else {
-        ## See how PKG_CPPFLAGS is defined in Rhtslib/src/Makevars.common
+        ## See how PKG_CPPFLAGS is defined in Rhtslib12/src/Makevars.common
         ## and make sure to produce the same value here.
         config <- "-D_FILE_OFFSET_BITS=64"
-        ## Packages that link to Rhtslib should have Rhtslib in their
+        ## Packages that link to Rhtslib12 should have Rhtslib12 in their
         ## LinkingTo field so the preprocessor option below will automatically
         ## be added. There is no need to add it again here.
-        #include_dir <- system.file("include", package="Rhtslib")
+        #include_dir <- system.file("include", package="Rhtslib12")
         #config <- paste(config, sprintf("-I'%s'", include_dir))
     }
     cat(config)
@@ -56,13 +56,13 @@ pkgconfig <- function(opt=c("PKG_LIBS", "PKG_CPPFLAGS"))
 
 htsVersion <- function()
 {
-    vers <- .Call("Rhtslib_htslib_version", PACKAGE="Rhtslib")
+    vers <- .Call("Rhtslib_htslib_version", PACKAGE="Rhtslib12")
     message(vers)
 }
 
 .onAttach <- function(...)
 {
-    vers <- .Call("Rhtslib_htslib_version", PACKAGE="Rhtslib")
-    packageStartupMessage("Rhtslib htslib version ", vers)
+    vers <- .Call("Rhtslib_htslib_version", PACKAGE="Rhtslib12")
+    packageStartupMessage("Rhtslib12 htslib version ", vers)
 }
 
